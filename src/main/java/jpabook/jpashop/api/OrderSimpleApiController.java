3b31e7ab8326adf491e07372789c2dfc29da32cc
soochan <tncksdl05@gmail.com>
2022-07-29 02:51:35 +0900

    feat : add order search api v2
    엔티티를 조회해서 필요한 데이터로 구성된 DTO로 변환 했지만
    N + 1의 문제점 발생 -> 추후 fetch join으로 해결

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,19 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * xToOne(ManyToOne, OneToOne) 관계 최적화를 하려고 한다. -> xToOne인 관계에 대해서만 가져오려고 한다.
@@ -40,4 +46,47 @@ public class OrderSimpleApiController {
             }
         return all;
     }
+
+
+    /**
+     * V2. 엔티티를 조회해서 DTO로 변환(fetch join 사용X)
+     * - 원래는 추후 유지보수 성을 위해서 Result<T>로 감싸야 하지만 지금은 그렇게 X
+     * - lazy 로딩으로 인한 데이터베이스 쿼리가 너무 많이 호출된다.
+     * - 3개의 Entity를 건들게 된다. 결과적으로 3개의 데이블을 조회해야 하는 상황
+     * - 처음에 Order에 대해서 Table 조회 -> 2개의 주문건수 확인
+     *                                    -> 루프로 1개의 건수 확인 -> Member, Delivery 조회
+     *                                    -> 루프로 1개의 건수 확인 -> Member, Delivery 조회
+     *
+     * - 총 5번의 쿼리가 나갔다.
+     * - N + 1 문제 발생
+     *
+     * - 원래 우리 예상은 1번 Order 조회 2번 주문 건수 조회 -> 3개를 예상했지만
+     *                 1번 Order 조회 -> 주문건수 마다 Member, Delivery 조회 -> 총 5개의 주문건수 확인
+     *
+     * - 단점: 지연로딩으로 쿼리 N번 호출 -> Fetch Join을 사용해야 한다.
+     */
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o)) // 객체를 생성자로 바로 넘긴다.
+                .collect(toList());
+        return result;
+    }
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate; //주문시간
+        private OrderStatus orderStatus;
+        private Address address;
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); // LAZY 초기화 , 프록시 객체가 아닌 실제 데이터를 담아서 넣는다. 원래는 Member가 lazy fetch이다.
+            orderDate = order.getOrderDate(); // LAZY 초기화 , 프록시 객체만을 반환하면 Error가 발생한다.따라서 실제 데이터를 반환해야한다.
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
+    }
+
 }
